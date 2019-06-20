c1b5856f9f6b50e632af4e73f97d310c75d1b7c3
hsolbrig <solbrig@jhu.edu>
2019-06-20 13:31:52 -0500

    Add URL's for each cohort

--- examples/list_profiles.py
+++ examples/list_profiles.py
@@ -1,19 +1,23 @@
+from typing import List, Tuple
+
 import requests
 from jsonasobj import loads
 
 url = "http://hapi.clinicalprofiles.org/baseR4/ClinicalProfile?_format=json&_count=10"
-ids = []
+ids:List[Tuple[str, str]] = []      # Tuples - cohort id to URL
 while True:
     resp = requests.get(url)
     url = None
     if resp.status_code == 200:
         rslts = loads(resp.text)
-        ids += [e.resource.id for e in rslts.entry]
+        ids += [(e.resource.id, e.fullUrl) for e in rslts.entry]
         for l in rslts.link:
             if l.relation == "next":
                 url = l.url
+    else:
+        print(resp.reason)
     if not url:
         break
 
 print(f"Count: {len(ids)}")
-print('\n'.join(sorted(ids)))
+print('\n'.join([e[0] + '\t' + e[1] for e in ids]))
